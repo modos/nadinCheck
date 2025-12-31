7357a60212b8a82267c2e132dbb3d8f4c98dd418
modos <modos.m98@gmail.com>
2026-01-01 01:30:06 +0330

    feat(package): add vuetify

---
+++ app/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+      <v-btn>test</v-btn>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/test-utils', '@nuxt/hints']
+  modules: ['@nuxt/eslint', '@nuxt/test-utils', '@nuxt/hints'],
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    plugins: [
+      vuetify({ autoImport: true }),
+    ],
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
 })
